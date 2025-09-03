24f470945807823b9e08aac4be0aad818090db2c
aadarshraj4321 <aadarshraj4321@gmail.com>
2025-09-04 00:06:46 +0530

    adding the render file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,37 @@
+// import axios from 'axios';
+
+// const apiClient = axios.create({
+//   baseURL: 'http://localhost:8000', 
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// export const createJob = (ticker) => {
+//   return apiClient.post('/jobs', { ticker });
+// };
+
+// export const getJob = (jobId) => {
+//   return apiClient.get(`/jobs/${jobId}`);
+// };
+
+
+// export const getJobsHistory = () => {
+//   return apiClient.get('/jobs');
+// };
+
+
+
+
+
 import axios from 'axios';
 
+// this line will use the production URL when deployed,
+// and the local URL when you're running it on your machine.
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000', 
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -14,7 +44,6 @@ export const createJob = (ticker) => {
 export const getJob = (jobId) => {
   return apiClient.get(`/jobs/${jobId}`);
 };
-
 
 export const getJobsHistory = () => {
   return apiClient.get('/jobs');
