1233552c69246f806d8e38c2c22feb58e030b3ff
aadarshraj4321 <aadarshraj4321@gmail.com>
2025-09-07 11:38:14 +0530

    hugging face redi bug fixed

--- startup.sh
+++ startup.sh
@@ -1,19 +1,4 @@
-# #!/bin/bash
-
-# # Start the Redis server in the background
-# redis-server --daemonize yes
-
-# # Start the Celery worker in the background
-# # We point it to the app folder where the code lives
-# celery -A celery_worker.celery worker --loglevel=info &
-
-# # Start the FastAPI server in the foreground
-# # This is the main process that keeps the container alive
-# # It must listen on 0.0.0.0 and port 7860 for Hugging Face Spaces
-# uvicorn main:app --host 0.0.0.0 --port 7860
-
-
-#!/bin/bash
+#!/bin/sh
 
 # Start the Redis server in the background WITH PERSISTENCE DISABLED
 # The '--save ""' command tells Redis not to create any RDB snapshots.
